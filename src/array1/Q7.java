dc0d614ea3ffd48adf0fa3d8e0acf37cf37e4837
Santosh Shah <santoshofficial679@gmail.com>
2023-07-31 22:50:28 +0545

    ...

---
+++ src/array1/Q7.java
@@ -0,0 +1,27 @@
+package array1;
+
+public class Q7 {
+
+    // printing array
+    public static void printArray(int[] arr) {
+        for (int ans : arr) {
+            System.out.print(ans + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // TODO: swap all pairs with its alternative and then print all element
+        int[] arr = {1, 2, 3, 6, 8};
+        swapPairsAlternatives(arr);
+        printArray(arr);
+    }
+
+    public static void swapPairsAlternatives(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length - 1; i = i + 2) {
+            temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+}
