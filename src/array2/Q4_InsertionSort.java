6d7903f2fbb71ec0eff68c99e337fe7d0c36dcb5
Santosh Shah <santoshofficial679@gmail.com>
2023-08-03 00:55:36 +0545

    Q4 and Q5 added

---
+++ src/array2/Q4_InsertionSort.java
@@ -0,0 +1,7 @@
+package array2;
+
+public class Q4_InsertionSort {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/array2/Q4_MergeTwoSortedArray.java
@@ -0,0 +1,7 @@
+package array2;
+
+public class Q4_MergeTwoSortedArray {
+    public static void main(String[] args) {
+
+    }
+}
