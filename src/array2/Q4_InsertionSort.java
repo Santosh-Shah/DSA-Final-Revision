9a79c5c31b4224c8e3a5b8ae70f82ea7701aeeb5
Santosh Shah <santoshofficial679@gmail.com>
2023-08-05 23:21:12 +0545

    Q4 insertion sort code is modified

--- src/array2/Q4_InsertionSort.java
+++ src/array2/Q4_InsertionSort.java
@@ -1,7 +1,26 @@
 package array2;
 
 public class Q4_InsertionSort {
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
     public static void main(String[] args) {
+        int[] arr = {1,4, 6, 8, 2, 90};
+        insertionSort(arr);
+        printArray(arr);
+    }
 
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i - 1;
+            int temp = arr[i];
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
     }
 }
