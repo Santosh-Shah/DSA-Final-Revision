4100fa0c210da505356e00404a68498e0be06df6
Santosh Shah <santoshofficial679@gmail.com>
2023-08-11 22:43:17 +0545

    Q6 calculate boundries and diagonal sum

---
+++ src/array3/Q6_BoundriesAndDiagonalSum.java
@@ -0,0 +1,31 @@
+package array3;
+
+public class Q6_BoundriesAndDiagonalSum {
+    public static void main(String[] args) {
+        //TODO: print largest column sum
+        int[][] arr = {
+                {1, 3, 2},
+                {4, 5, 6},
+                {7, 9, 8}
+        };
+
+        boundriesAndDiagonalSum(arr);
+    }
+
+    public static void boundriesAndDiagonalSum(int[][] arr) {
+        int len = arr.length;
+        int totalSum = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                // sum for all diagonals
+                if (i == j || (i + j) == len - 1) {
+                    totalSum = totalSum + arr[i][j];
+                } else if (i == 0 || j == 0 || i == len - 1 || j == len - 1) {
+                    totalSum = totalSum + arr[i][j];
+                }
+            }
+        }
+
+        System.out.println(totalSum);
+    }
+}
