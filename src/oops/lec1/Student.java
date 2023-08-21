c3e0c1895cf2724e752b827d7b09e2c824bd57aa
Santosh Shah <santoshofficial679@gmail.com>
2023-08-21 17:58:57 +0545

    Created class with two constructor

---
+++ src/oops/lec1/Student.java
@@ -0,0 +1,25 @@
+package oops.lec1;
+
+public class Student {
+
+    // attributes of student
+    String name;
+    int age;
+    String phoneNo;
+
+    // constructor to print student details
+    Student(String name, int age, String phoneNo) {
+        System.out.println("My name is: " + name + "\n" + "age: " + age + "\n" + "Phone Number: " + phoneNo);
+    }
+
+    // main methods
+    public static void main(String[] args) {
+        Student std1 = new Student("Hariom Shah", 20, "9811243552");
+        System.out.println("---------------------------------");
+
+        Student std2 = new Student("Santosh Shah", 23, "9849697391");
+        System.out.println("---------------------------------");
+
+        Student std3 = new Student("Maneesh Shah", 24, "9825263516");
+    }
+}
