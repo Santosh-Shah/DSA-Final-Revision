40fd23e4467302350022d2015cd316cb115d45dd
Santosh Shah <santoshofficial679@gmail.com>
2023-09-03 17:54:23 +0545

    Q4 added

---
+++ src/recursion/Q4_FindNumberOfDigits.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class Q4_FindNumberOfDigits {
+    // Recursive way
+    public static int countRecursively(int num) {
+        // Base case
+        if (num / 10 == 0) {
+            return 1;
+        }
+
+        int smallOutput = countRecursively(num / 10);
+        return smallOutput + 1;
+    }
+    public static void main(String[] args) {
+        System.out.println(countRecursively(123));
+    }
+}
