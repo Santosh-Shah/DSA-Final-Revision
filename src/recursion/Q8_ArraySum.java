55e46dd68ed3840bc7c21d2cd8bc72ce412e3823
Santosh Shah <santoshofficial679@gmail.com>
2023-09-05 14:28:55 +0545

    Q8 completed

---
+++ src/recursion/Q8_ArraySum.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class Q8_ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 2, 70};
+        System.out.println(arraySum(arr));
+    }
+
+    // Array sum
+    public static int arraySum(int[] arr) {
+        return sum(arr, 0);
+    }
+
+    public static int sum(int[] arr, int startIndex) {
+        if (startIndex == arr.length) {
+            return 0;
+        }
+        int smallOutput = sum(arr, startIndex + 1);
+        return arr[startIndex] + smallOutput;
+    }
+}
