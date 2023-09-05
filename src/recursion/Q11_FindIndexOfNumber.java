fd0373a5fc1473e29cb3f502150d5c5900e426b5
Santosh Shah <santoshofficial679@gmail.com>
2023-09-05 15:30:35 +0545

    Q11 solved

---
+++ src/recursion/Q11_FindIndexOfNumber.java
@@ -0,0 +1,26 @@
+package recursion;
+
+public class Q11_FindIndexOfNumber {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 9, 10};
+        System.out.println(findIndex(arr, 11));
+    }
+
+    // find index of present number
+    public static int findIndex(int[] arr, int target) {
+        return findIndex(arr, target, 0);
+    }
+
+    public static int findIndex(int[] arr, int target, int si) {
+        // Base case
+        if (si == arr.length) {
+            return -1;
+        }
+
+        if (arr[si] == target) {
+            return si;
+        }
+
+        return findIndex(arr, target, si + 1);
+    }
+}
