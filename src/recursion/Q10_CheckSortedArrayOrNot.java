23ab5dfc02d68123ed1855cc0fbea64e30747a03
Santosh Shah <santoshofficial679@gmail.com>
2023-09-05 15:16:17 +0545

    Q10 solved

---
+++ src/recursion/Q10_CheckSortedArrayOrNot.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class Q10_CheckSortedArrayOrNot {
+    public static void main(String[] args) {
+        int[] arr = {5, 60, 7, 8};
+        System.out.println(isSortedBetter(arr, 0));
+    }
+
+    // Better version to check sorted array or not
+    public static boolean isSortedBetter(int[] arr, int startIndex) {
+        if (startIndex == arr.length - 1) {
+            return true;
+        }
+
+        if (arr[startIndex] > arr[startIndex + 1]) {
+            return false;
+        }
+
+        return isSortedBetter(arr, startIndex + 1);
+    }
+}
