6d7bb4b1c3366603a47a899af95e3423e59cb9c4
Santosh Shah <santoshofficial679@gmail.com>
2023-09-07 18:49:32 +0545

    Q9 tower of hanoi solved

---
+++ src/recursion/part2/Q9_TowerHanoi.java
@@ -0,0 +1,18 @@
+package recursion.part2;
+
+public class Q9_TowerHanoi {
+    public static void main(String[] args) {
+        towerHanoi(4, 's', 'h', 'd');
+    }
+
+    // Method to solved tower of hanoi
+    public static void towerHanoi(int n, char s, char h, char d) {
+        if (n == 1) {
+            System.out.println("move 1st disk from " + s + " to " + d);
+            return;
+        }
+        towerHanoi(n - 1, s, d, h);
+        System.out.println("move " + n + "th disk from " + s + " to " + d);
+        towerHanoi(n - 1, h, s, d);
+    }
+}
