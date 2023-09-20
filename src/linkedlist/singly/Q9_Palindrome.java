8b7bdca5a62c25063762a2dc92169ba07655d6f9
Santosh Shah <santoshofficial679@gmail.com>
2023-09-20 21:06:59 +0545

    Q9 solved

--- src/linkedlist/singly/Q9_Palindrome.java
+++ src/linkedlist/singly/Q9_Palindrome.java
@@ -1,7 +1,103 @@
 package linkedlist.singly;
 
+import java.util.Scanner;
+
 public class Q9_Palindrome {
+    // Method to take input
+    public static Node<Integer> takeInput() {
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        Node<Integer> head = null, tail = null;
+        while (data != -1) {
+            Node<Integer> currentNode = new Node<>(data);
+            if (head == null) {
+                head = currentNode;
+                tail = currentNode;
+            } else {
+                // it will take O(1) TC
+                tail.next = currentNode;
+                tail = tail.next;
+
+            }
+
+            data = sc.nextInt();
+        }
+        return head;
+    }
+
+    // method to print linked list
+    public static void printNode(Node<Integer> head) {
+        // Base case
+        if (head == null) {
+            return;
+        }
+
+        Node<Integer> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+//        System.out.println(head.data);
+//        System.out.println(head.next.next.next.data);
+//        System.out.println(head.next.next.next.next);
+    }
+
+    // TODO: method to check palindrome
+    public static boolean isPalindrome(Node<Integer> head) {
+        //Your code goes here
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // step-1: find the middle of the linked list
+        Node<Integer> fast = head;
+        Node<Integer> slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // step-2: reverse the second half of the linked list
+        Node<Integer> secondHalf = reverse(slow);
+
+        // step-3: now compare fist half with reverse second half
+        Node<Integer> firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+
+            secondHalf = secondHalf.next;
+            firstHalf = firstHalf.next;
+        }
+        return true;
+    }
+
+    public static Node<Integer> reverse(Node<Integer> head) {
+        Node<Integer> prev = null;
+        Node<Integer> current = head;
+
+        while (current != null) {
+            Node<Integer> next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+
+
+    // Main Method
     public static void main(String[] args) {
+
+        // insert node at ith position
+        Node<Integer> head = takeInput();
+        printNode(head);
+        System.out.println(isPalindrome(head));
 
     }
 }
