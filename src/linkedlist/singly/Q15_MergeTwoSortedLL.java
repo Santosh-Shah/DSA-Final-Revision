2512975fb140c98b56de580800a4e0c3d56b2cb9
Santosh Shah <santoshofficial679@gmail.com>
2023-09-20 13:22:10 +0545

    Q15 solved

--- src/linkedlist/singly/Q15_MergeTwoSortedLL.java
+++ src/linkedlist/singly/Q15_MergeTwoSortedLL.java
@@ -52,20 +52,53 @@ public class Q15_MergeTwoSortedLL {
 
     // TODO: method to merge two sorted LL
     public static Node<Integer> mergeTwoSortedLL(Node<Integer> head1, Node<Integer> head2) {
+        // Base case
+        if (head1 == null) {
+            return head2;
+        }
+
+        if (head2 == null) {
+            return head1;
+        }
 
         Node<Integer> temp1 = head1;
         Node<Integer> temp2 = head2;
-        Node<Integer> currentHead = temp1;
         Node<Integer> head;
+        Node<Integer> tail;
 
         if (temp1.data <= temp2.data) {
-            currentHead = temp1;
-            temp1= temp1.next;
+            head = temp1;
+            tail = temp1;
+            temp1 = temp1.next;
         } else {
-            currentHead = temp2;
+            head = temp2;
+            tail = temp2;
             temp2 = temp2.next;
         }
-        return currentHead;
+
+        while (temp1 != null && temp2 != null) {
+            if (temp1.data <= temp2.data) {
+                tail.next = temp1;
+                tail = temp1;
+                temp1 = temp1.next;
+            } else {
+                tail.next = temp2;
+                tail = temp2;
+                temp2 = temp2.next;
+            }
+        }
+
+        // this will only temp1 has still nodes
+        if (temp1 != null) {
+            tail.next = temp1;
+        }
+
+        // this will only temp2 has still nodes
+        if (temp2 != null) {
+            tail.next = temp2;
+        }
+
+        return head;
     }
 
 
