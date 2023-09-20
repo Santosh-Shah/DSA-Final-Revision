6d736f0878a5834ba4262c0c28d17b2b095834ac
Santosh Shah <santoshofficial679@gmail.com>
2023-09-20 21:00:27 +0545

    Q22 solved

---
+++ src/linkedlist/singly/Q22_BubbleSort.java
@@ -0,0 +1,106 @@
+package linkedlist.singly;
+
+import java.util.Scanner;
+
+public class Q22_BubbleSort {
+    // Method to take input
+    public static Node<Integer> takeInput() {
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        Node<Integer> head = null, tail = null;
+        while (data != -1) {
+            Node<Integer> currentNode = new Node<>(data);
+            if (head == null) {
+                head = currentNode;
+                tail = currentNode;
+            } else {
+                // it is not optimized code
+//                Node<Integer> tail = head;
+//                while (tail.next != null) {
+//                    tail = tail.next;
+//                }
+//                tail.next = currentNode;
+
+                // it will take O(1) TC
+                tail.next = currentNode;
+                tail = tail.next;
+
+            }
+
+            data = sc.nextInt();
+        }
+        return head;
+    }
+
+    // method to print linked list
+    public static void printNode(Node<Integer> head) {
+        // Base case
+        if (head == null) {
+            return;
+        }
+
+        Node<Integer> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+//        System.out.println(head.data);
+//        System.out.println(head.next.next.next.data);
+//        System.out.println(head.next.next.next.next);
+    }
+
+    // TODO: method of bubble sort in LL
+    public static Node<Integer> bubbleSort(Node<Integer> head) {
+        int n = length(head);
+        for (int i = 0; i < (n - 1); i++) {
+            Node<Integer> prev = null;
+            Node<Integer> curr = head;
+
+            for (int j = 0; j < (n - i - 1); j++) {
+                if (curr.data <= curr.next.data) {
+                    prev = curr;
+                    curr = curr.next;
+                } else {
+                    if (prev == null) {
+                        Node<Integer> fwd = curr.next;
+                        head = head.next;
+                        curr.next = fwd.next;
+                        fwd.next = curr;
+                        prev = fwd;
+                    } else {
+                        Node<Integer> fwd = curr.next;
+                        prev.next = fwd;
+                        curr.next = fwd.next;
+                        fwd.next = curr;
+                        prev = fwd;
+                    }
+                }
+            }
+        }
+
+        return head;
+    }
+
+    // method to find length
+    public static int length(Node<Integer> head) {
+        if (head == null) {
+            return 0;
+        }
+        int count = length(head.next);
+        count++;
+        return count;
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+
+        // insert node at ith position
+        Node<Integer> head = takeInput();
+        printNode(head);
+        head = bubbleSort(head);
+        System.out.println();
+        printNode(head);
+
+    }
+}
