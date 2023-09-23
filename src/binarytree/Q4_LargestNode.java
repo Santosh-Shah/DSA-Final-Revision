bb81d978b2d29228e36393459defcbae485b30e4
Santosh Shah <santoshofficial679@gmail.com>
2023-09-23 23:25:59 +0545

    Q4 solved

---
+++ src/binarytree/Q4_LargestNode.java
@@ -0,0 +1,70 @@
+package binarytree;
+
+import java.util.Scanner;
+
+public class Q4_LargestNode {
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = takeInputBetter(true, 0, false);
+        printDetailed(root);
+        System.out.println("Largest node is: " + largestNode(root));
+    }
+
+    // TODO: method to that return largest node
+    public static int largestNode(BinaryTreeNode<Integer> root) {
+        // Base case
+        if (root == null) {
+            return -1;
+        }
+
+        int leftNode = largestNode(root.left);
+        int rightNode = largestNode(root.right);
+        return Math.max(root.data, Math.max(leftNode, rightNode));
+    }
+
+    // method to take input in better way
+    public static BinaryTreeNode<Integer> takeInputBetter(boolean isRoot, int parentData, boolean isLeft) {
+        if (isRoot) {
+            System.out.println("Enter root data: ");
+        } else {
+            if (isLeft) {
+                System.out.println("Enter left child of: " + parentData);
+            } else {
+                System.out.println("Enter right child of: " + parentData);
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int rootData = sc.nextInt();
+
+        if (rootData == -1) {
+            return null;
+        }
+
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+        BinaryTreeNode<Integer> rootLeft = takeInputBetter(false, rootData, true);
+        BinaryTreeNode<Integer> rootRight = takeInputBetter(false, rootData, false);
+        root.left = rootLeft;
+        root.right = rootRight;
+        return root;
+    }
+
+    // Methods to print binary tree in detailed
+    public static void printDetailed(BinaryTreeNode<Integer> root) {
+        // Base case
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + ": ");
+        if (root.left != null) {
+            System.out.print("L" + root.left.data + ", ");
+        }
+
+        if (root.right != null) {
+            System.out.print("R" + root.right.data);
+        }
+        System.out.println();
+        printDetailed(root.left);
+        printDetailed(root.right);
+    }
+
+}
