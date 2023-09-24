ec1d5cbdfe3e1299b854c8075878cef2cc5f4649
Santosh Shah <santoshofficial679@gmail.com>
2023-09-24 17:04:54 +0545

    Q14 solved

---
+++ src/binarytree/Q14_IsBalanced.java
@@ -0,0 +1,84 @@
+package binarytree;
+
+import java.util.Scanner;
+
+public class Q14_IsBalanced {
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = takeInputBetter(true, 0, false);
+        printDetailed(root);
+        System.out.println("Is tree balanced: " + isBalanced(root));
+    }
+
+    // TODO: method to check is tree balanced or not
+    public static boolean isBalanced(BinaryTreeNode<Integer> root) {
+        // Base case
+       if (root == null) {
+           return true;
+       }
+
+       int leftHeight = height(root.left);
+       int rightHeight = height(root.right);
+       if (Math.abs(leftHeight - rightHeight) > 1) {
+           return false;
+       }
+
+       boolean leftBalanced = isBalanced(root.left);
+       boolean rightBalanced = isBalanced(root.right);
+
+       return leftBalanced && rightBalanced;
+    }
+
+    public static int height(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    // method to take input in better way
+    public static BinaryTreeNode<Integer> takeInputBetter(boolean isRoot, int parentData, boolean isLeft) {
+        if (isRoot) {
+            System.out.println("Enter root data: ");
+        } else {
+            if (isLeft) {
+                System.out.println("Enter left child of: " + parentData);
+            } else {
+                System.out.println("Enter right child of: " + parentData);
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int rootData = sc.nextInt();
+
+        if (rootData == -1) {
+            return null;
+        }
+
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+        BinaryTreeNode<Integer> rootLeft = takeInputBetter(false, rootData, true);
+        BinaryTreeNode<Integer> rootRight = takeInputBetter(false, rootData, false);
+        root.left = rootLeft;
+        root.right = rootRight;
+        return root;
+    }
+
+    // Methods to print binary tree in detailed
+    public static void printDetailed(BinaryTreeNode<Integer> root) {
+        // Base case
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + ": ");
+        if (root.left != null) {
+            System.out.print("L" + root.left.data + ", ");
+        }
+
+        if (root.right != null) {
+            System.out.print("R" + root.right.data);
+        }
+        System.out.println();
+        printDetailed(root.left);
+        printDetailed(root.right);
+    }
+
+}
