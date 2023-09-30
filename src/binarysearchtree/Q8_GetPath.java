dd15f057c7e085bbcddbbad3d9f6c85bc4733fa6
Santosh Shah <santoshofficial679@gmail.com>
2023-09-30 15:07:15 +0545

    Q8 done

---
+++ src/binarysearchtree/Q8_GetPath.java
@@ -0,0 +1,149 @@
+package binarysearchtree;
+
+import binarytree.BinaryTreeNode;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q8_GetPath {
+    public static void main(String[] args) {
+//        BinaryTreeNode<Integer> root = takeInputBetter(true, 0, false);
+        int[] inOrder = {1, 2, 3, 4, 5, 6, 7};
+        int[] preOrder = {4, 2, 1, 3, 6, 5, 7};
+        BinaryTreeNode<Integer> root = buildTree(preOrder, inOrder);
+        printDetailed(root);
+        ArrayList<Integer> arr = getPath(root, 50);
+        if (arr == null) {
+            System.out.println("Target value not found");
+        } else {
+            for (int ans : arr) {
+                System.out.print(ans + " ");
+            }
+        }
+    }
+
+    // TODO: method to path of targeted node or value
+    public static ArrayList<Integer> getPath(BinaryTreeNode<Integer> root, int target) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.data == target) {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(root.data);
+            return list;
+        } else if (root.data > target) {
+            ArrayList<Integer> list = getPath(root.left, target);
+            if (list != null) {
+                list.add(root.data);
+                return list;
+            }
+        }
+
+        ArrayList<Integer> list;
+        if (root.data < target) {
+            list = getPath(root.right, target);
+            if (list != null) {
+                list.add(root.data);
+                return list;
+            }
+        }
+
+        return null;
+    }
+
+    // method to build tree using pre_order and in_order
+    public static BinaryTreeNode<Integer> buildTree(int[] pre, int[] in) {
+        BinaryTreeNode<Integer> root = buildTreeFromPreInHelper(pre, in, 0, pre.length - 1, 0, in.length - 1);
+        return root;
+    }
+
+    private static BinaryTreeNode<Integer> buildTreeFromPreInHelper(int[] pre, int[] in, int siPre, int eiPre, int siIn, int eiIn) {
+        if (siPre > eiPre) {
+            return null;
+        }
+
+
+        int rootData = pre[siPre];
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+
+        // find root index in in-order
+        int rootIndex = -1;
+        for (int i = siIn; i <= eiIn; i++) {
+            if (in[i] == rootData) {
+                rootIndex = i;
+                break;
+            }
+        }
+
+//        if (rootIndex == -1) {
+//            return null;
+//        }
+
+        int siPreLeft = siPre + 1;
+        int siInLeft = siIn;
+        int eiInLeft = rootIndex - 1;
+
+        int siInRight = rootIndex + 1;
+        int eiPreRight = eiPre;
+        int eiInRight = eiIn;
+
+        // finding length of left subtree
+        int leftSubTreeLength = eiInLeft - siInLeft + 1;
+
+        int eiPreLeft = siPreLeft + leftSubTreeLength - 1;
+        int siPreRight = eiPreLeft + 1;
+
+        BinaryTreeNode<Integer> left = buildTreeFromPreInHelper(pre, in, siPreLeft, eiPreLeft, siInLeft, eiInLeft);
+        BinaryTreeNode<Integer> right = buildTreeFromPreInHelper(pre, in, siPreRight, eiPreRight, siInRight, eiInRight);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+
+    // method to take input in better way
+    public static BinaryTreeNode<Integer> takeInputBetter(boolean isRoot, int parentData, boolean isLeft) {
+        if (isRoot) {
+            System.out.println("Enter root data: ");
+        } else {
+            if (isLeft) {
+                System.out.println("Enter left child of: " + parentData);
+            } else {
+                System.out.println("Enter right child of: " + parentData);
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int rootData = sc.nextInt();
+
+        if (rootData == -1) {
+            return null;
+        }
+
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+        BinaryTreeNode<Integer> rootLeft = takeInputBetter(false, rootData, true);
+        BinaryTreeNode<Integer> rootRight = takeInputBetter(false, rootData, false);
+        root.left = rootLeft;
+        root.right = rootRight;
+        return root;
+    }
+
+    // Methods to print binary tree in detailed
+    public static void printDetailed(BinaryTreeNode<Integer> root) {
+        // Base case
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + ": ");
+        if (root.left != null) {
+            System.out.print("L" + root.left.data + ", ");
+        }
+
+        if (root.right != null) {
+            System.out.print("R" + root.right.data);
+        }
+        System.out.println();
+        printDetailed(root.left);
+        printDetailed(root.right);
+    }
+}
