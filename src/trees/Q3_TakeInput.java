27adeaf625e705560a26a0ef411a6ffca47f1080
Santosh Shah <santoshofficial679@gmail.com>
2023-10-04 23:23:37 +0545

    Q4 solved

--- src/trees/Q3_TakeInput.java
+++ src/trees/Q3_TakeInput.java
@@ -5,28 +5,6 @@ import java.util.Scanner;
 
 public class Q3_TakeInput {
     public static void main(String[] args) {
-//        TreeNode<Integer> tree = new TreeNode<>(10);
-//        System.out.println(tree.data);
-//        System.out.println(tree.children.size());
-
-//        TreeNode<Integer> root = new TreeNode<>(4);
-//        TreeNode<Integer> node1 = new TreeNode<>(2);
-//        TreeNode<Integer> node2 = new TreeNode<>(3);
-//        TreeNode<Integer> node3 = new TreeNode<>(1);
-//        TreeNode<Integer> node4 = new TreeNode<>(5);
-//        TreeNode<Integer> node5 = new TreeNode<>(6);
-//
-//        // adding 2, 3, and 1 to root node
-//        root.children.add(node1);
-//        root.children.add(node2);
-//        root.children.add(node3);
-//
-//        // adding 5 and 6 to node2
-//        node2.children.add(node4);
-//        node2.children.add(node5);
-
-//        printTree(root);
-//        printTree2(root);
 
         TreeNode<Integer> root = takeInput();
         printTree2(root);
---
+++ src/trees/Q3_TakeInputLevelWise.java
@@ -0,0 +1,67 @@
+package trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Q3_TakeInputLevelWise {
+    public static void main(String[] args) {
+
+        TreeNode<Integer> root = takeInputLevelWise();
+        printTree2(root);
+
+
+    }
+
+    // TODO: method to take input level wise
+    public static TreeNode<Integer> takeInputLevelWise() {
+        Scanner sc = new Scanner(System.in);
+        Queue<TreeNode<Integer>> pendingNodes = new LinkedList<>();
+        System.out.println("Enter root data: ");
+        int rootData = sc.nextInt();
+
+        if (rootData == -1) {
+            return null;
+        }
+
+        TreeNode<Integer> root = new TreeNode<>(rootData);
+        pendingNodes.add(root);
+
+        while (!pendingNodes.isEmpty()) {
+            TreeNode<Integer> front = pendingNodes.poll();
+            System.out.println("Enter the no of Children for: " + front.data);
+            int numChild = sc.nextInt();
+            for (int i = 0; i < numChild; i++) {
+                System.out.println("Enter the " + i + "th child data for " + front.data);
+                int childData = sc.nextInt();
+                TreeNode<Integer> childNode = new TreeNode<>(childData);
+                front.children.add(childNode);
+                pendingNodes.add(childNode);
+            }
+        }
+
+        return root;
+    }
+
+
+    // TODO: method2 to print generic tree
+    public static void printTree2(TreeNode<Integer> root) {
+        // it is special case, base case is already handle by for loop
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + ": ");
+
+        // printing all children
+        for (int i = 0; i < root.children.size(); i++) {
+            System.out.print(root.children.get(i).data + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < root.children.size(); i++) {
+            TreeNode<Integer> child = root.children.get(i);
+            printTree2(child);
+        }
+    }
+}
