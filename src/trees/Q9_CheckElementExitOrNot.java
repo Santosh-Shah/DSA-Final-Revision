fc5b110cc64445514f421a335a79df64b091a2b2
Santosh Shah <santoshofficial679@gmail.com>
2023-10-07 23:47:48 +0545

    Q9 solved

---
+++ src/trees/Q9_CheckElementExitOrNot.java
@@ -0,0 +1,68 @@
+package trees;
+
+public class Q9_CheckElementExitOrNot {
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(4);
+        TreeNode<Integer> node1 = new TreeNode<>(2);
+        TreeNode<Integer> node2 = new TreeNode<>(3);
+        TreeNode<Integer> node3 = new TreeNode<>(1);
+        TreeNode<Integer> node4 = new TreeNode<>(5);
+        TreeNode<Integer> node5 = new TreeNode<>(6);
+
+        // adding 2, 3, and 1 to root node
+        root.children.add(node1);
+        root.children.add(node2);
+        root.children.add(node3);
+
+        // adding 5 and 6 to node2
+        node2.children.add(node4);
+        node2.children.add(node5);
+
+
+//        printTreeLevelWise(root);
+        printTree2(root);
+        System.out.println("Is x exit: " + checkXExitOrNot(root, 5));
+
+
+    }
+
+    // TODO: method to check X exit or not
+    public static boolean checkXExitOrNot(TreeNode<Integer> root, int x) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == x) {
+            return true;
+        }
+
+        for (int i = 0; i < root.children.size(); i++) {
+            if (checkXExitOrNot(root.children.get(i), x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // TODO: method2 to print generic tree
+    public static void printTree2(TreeNode<Integer> root) {
+        // it is special case, base case is already handle by for loop
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + ": ");
+
+        // printing all children
+        for (int i = 0; i < root.children.size(); i++) {
+            System.out.print(root.children.get(i).data + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < root.children.size(); i++) {
+            TreeNode<Integer> child = root.children.get(i);
+            printTree2(child);
+        }
+    }
+}
