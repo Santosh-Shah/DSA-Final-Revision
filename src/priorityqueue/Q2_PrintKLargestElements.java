1a04bda9dd24441291be4c592460caa810ddc0ff
Santosh Shah <santoshofficial679@gmail.com>
2023-10-14 16:39:54 +0545

    Q2 solved

---
+++ src/priorityqueue/Q2_PrintKLargestElements.java
@@ -0,0 +1,37 @@
+package priorityqueue;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Q2_PrintKLargestElements {
+    public static void main(String[] args) {
+        int[] arr = {4, 7, 3, 2, 8, 9, 6, 1};
+        ArrayList<Integer> ans = printKLargestElements(arr, 4);
+        System.out.println(ans);
+    }
+
+    public static ArrayList<Integer> printKLargestElements(int[] arr, int k) {
+        if (arr.length == 0) {
+            return null;
+        }
+        // Build the heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            pq.add(arr[i]);
+        }
+
+        for (int i = k; i < arr.length; i++) {
+            if (pq.peek() < arr[i]) {
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ans.add(pq.poll());
+        }
+
+        return ans;
+    }
+}
