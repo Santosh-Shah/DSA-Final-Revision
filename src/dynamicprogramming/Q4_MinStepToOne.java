63411b7feba14632bbd496244a2f82ad8e6730c8
Santosh Shah <santoshofficial679@gmail.com>
2023-10-21 22:26:14 +0545

    Q4 solved

---
+++ src/dynamicprogramming/Q4_MinStepToOne.java
@@ -0,0 +1,26 @@
+package dynamicprogramming;
+
+public class Q4_MinStepToOne {
+    public static void main(String[] args) {
+        System.out.println(countMinStepsToOne(5));
+    }
+
+    public static int countMinStepsToOne(int n) {
+        if (n == 1) {
+            return 0;
+        }
+
+        int ans1 = countMinStepsToOne(n - 1);
+        int ans2 = Integer.MAX_VALUE;
+        if ((n % 2) == 0) {
+            ans2 = countMinStepsToOne(n / 2);
+        }
+
+        int ans3 = Integer.MAX_VALUE;
+        if (n % 3 == 0) {
+            ans3 = countMinStepsToOne(n / 3);
+        }
+
+        return Math.min(ans1, Math.min(ans2, ans3)) + 1;
+    }
+}
