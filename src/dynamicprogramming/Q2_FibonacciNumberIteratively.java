f9cdc5ab6790d9f2892b82b1be40a45f429c5aaa
Santosh Shah <santoshofficial679@gmail.com>
2023-10-21 16:57:42 +0545

    Q2 solved

--- src/dynamicprogramming/Q2_FibonacciNumberIteratively.java
+++ src/dynamicprogramming/Q2_FibonacciNumberIteratively.java
@@ -2,35 +2,29 @@ package dynamicprogramming;
 
 public class Q2_FibonacciNumberIteratively {
     public static void main(String[] args) {
-        int n = 7;
-        int[] dp = new int[n + 1];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = -1;
-        }
+        int n = 6;
+//        int[] dp = new int[n + 1];
+//        for (int i = 0; i < dp.length; i++) {
+//            dp[i] = -1;
+//        }
 
-        System.out.println(fibonacci(n, dp));
+        System.out.println(fibonacci(n));
     }
 
-    public static int fibonacci(int n, int[] dp) {
+    public static int fibonacci(int n) {
         if (n == 0 || n == 1) {
             return n;
         }
 
-        int ans1, ans2;
-        if (dp[n - 1] == -1) {
-            ans1 = fibonacci(n - 1, dp);
-            dp[n - 1] = ans1;
-        } else {
-            ans1 = dp[n - 1];
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
-        if (dp[n - 2] == -1) {
-            ans2 = fibonacci(n - 2, dp);
-            dp[n - 2] = ans2;
-        } else {
-            ans2 = dp[n - 2];
-        }
+        return dp[n];
 
-        return ans1 + ans2;
+
     }
 }
