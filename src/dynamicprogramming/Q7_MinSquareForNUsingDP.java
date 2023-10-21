3109f9736146c51f3b94cb6661fc8e09efda484d
Santosh Shah <santoshofficial679@gmail.com>
2023-10-22 00:07:04 +0545

    in Q7 Iterative way add

--- src/dynamicprogramming/Q7_MinSquareForNUsingDP.java
+++ src/dynamicprogramming/Q7_MinSquareForNUsingDP.java
@@ -3,13 +3,16 @@ package dynamicprogramming;
 public class Q7_MinSquareForNUsingDP {
     public static void main(String[] args) {
         int n = 12;
-        int[] dp = new int[n + 1];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = -1;
-        }
-        System.out.println(minSquares(n, dp));
-    }
+//        int[] dp = new int[n + 1];
+//        for (int i = 0; i < dp.length; i++) {
+//            dp[i] = -1;
+//        }
+//        System.out.println(minSquares(n, dp));
 
+        System.out.println(minSquareIteratively(n));
+        }
+
+    // Recursive way
     public static int minSquares(int n, int[] dp) {
         if (n == 0) {
             return 0;
@@ -31,5 +34,22 @@ public class Q7_MinSquareForNUsingDP {
         }
         int myAns = 1 + minAns;
         return myAns;
+    }
+
+    // Iterative way
+    public static int minSquareIteratively(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int minAns = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                int currAns = dp[i - (j * j)];
+                if (minAns > currAns) {
+                    minAns = currAns;
+                }
+            }
+            dp[i] = 1 + minAns;
+        }
+        return dp[n];
     }
 }
