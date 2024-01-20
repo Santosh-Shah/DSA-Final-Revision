3ebb94c26288a105ec09a8c0c475fcb2bc97eb0e
Santosh Shah <santoshofficial679@gmail.com>
2024-01-21 00:30:16 +0545

    Q9 solved

---
+++ src/revision_once/linkedlists/singly/Q9_PrintRecursively.java
@@ -0,0 +1,77 @@
+package revision_once.linkedlists.singly;
+
+import java.util.Scanner;
+
+public class Q9_PrintRecursively {
+    public static void main(String[] args) {
+        Node<Integer> head = createLL();
+//        Node<Integer> head = takeInput();
+//        printLL(head);
+        printRecursively(head);
+        System.out.println();
+        printLL(head);
+    }
+
+    public static void printRecursively(Node<Integer> head) {
+        // Base case
+        if (head == null) {
+            return;
+        }
+
+        Node<Integer> temp = head;
+        System.out.print(temp.data + " ");
+        printRecursively(temp.next);
+    }
+
+
+    //TODO: Take input
+    public static Node<Integer> takeInput() {
+        Node<Integer> tail = null;
+        Node<Integer> head = null;
+
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        while (data != -1) {
+            Node<Integer> currData = new Node<>(data);
+            if (head == null) {
+                head = currData;
+                tail = currData;
+            } else {
+                tail.next = currData;
+                tail = tail.next;
+            }
+
+            data = sc.nextInt();
+        }
+
+        return head;
+    }
+
+    public static void printLL(Node<Integer> head) {
+        // Base case
+        if (head == null) {
+            return;
+        }
+
+        Node<Integer> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+    }
+
+    public static Node<Integer> createLL() {
+        Node<Integer> n1 = new Node<>(10);
+        Node<Integer> n2 = new Node<>(20);
+        Node<Integer> n3 = new Node<>(30);
+        Node<Integer> n4 = new Node<>(40);
+        Node<Integer> n5 = new Node<>(50);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        return n1;
+    }
+}
