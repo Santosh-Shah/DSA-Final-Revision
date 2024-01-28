f774e3de6dcbd75d359f4f7470d76de886cf133f
Santosh Shah <santoshofficial679@gmail.com>
2024-01-28 15:49:21 +0545

    Q3 solved

--- src/revision_once/stacks/Q3_CheckRedundantBrackets.java
+++ src/revision_once/stacks/Q3_CheckRedundantBrackets.java
@@ -1,19 +1,22 @@
-package stack;
+package revision_once.stacks;
 
 import java.util.Stack;
 
 public class Q3_CheckRedundantBrackets {
-    // TODO: method to check redundant brackets
+    public static void main(String[] args) {
+        System.out.println(checkRedundantBrackets("(a)+ b - c"));
+    }
+
     public static boolean checkRedundantBrackets(String exp) {
-        Stack<Character> stk = new Stack<>();
-        for (int i = 0; i < exp.length(); ++i) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < exp.length(); i++) {
             if (exp.charAt(i) == '(' || find(exp.charAt(i))) {
-                stk.push(exp.charAt(i));
-            } else if (exp.charAt(i) == ')') {
+                stack.push(exp.charAt(i));
+            } else if(exp.charAt(i) == ')'){
                 boolean hasOperator = false;
 
-                while (!stk.isEmpty() && stk.peek() != '(') {
-                    stk.pop();
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    stack.pop();
                     hasOperator = true;
                 }
 
@@ -21,25 +24,19 @@ public class Q3_CheckRedundantBrackets {
                     return true;
                 }
 
-                if (!stk.isEmpty()) {
-                    stk.pop();
+                if (!stack.isEmpty()) {
+                    stack.pop();
                 }
             }
         }
         return false;
     }
 
-    private static boolean find(char ch) {
+    public static boolean find(char ch) {
         if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
             return true;
         }
 
         return false;
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println(checkRedundantBrackets("(a+a)+b"));
-
     }
 }
